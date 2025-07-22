9eb2fc546a0cb1be6df34b78d0435b3c13352f49
Guillaume Bouchard <guillaum.bouchard@gmail.com>
2025-07-22 10:42:14 +0400

    feat: add some HasCallStack in sydtest-wai

--- sydtest-wai/src/Test/Syd/Wai/Request.hs
+++ sydtest-wai/src/Test/Syd/Wai/Request.hs
@@ -16,34 +16,35 @@ import Network.HTTP.Types as HTTP
 import Test.Syd
 import Test.Syd.Wai.Client
 import Test.Syd.Wai.Matcher
+import Control.Exception (SomeException, try, throwIO)
 
 -- | Perform a @GET@ request to the application under test.
-get :: ByteString -> WaiSession st (HTTP.Response LB.ByteString)
+get :: HasCallStack => ByteString -> WaiSession st (HTTP.Response LB.ByteString)
 get path = request methodGet path [] ""
 
 -- | Perform a @POST@ request to the application under test.
-post :: ByteString -> LB.ByteString -> WaiSession st (HTTP.Response LB.ByteString)
+post :: HasCallStack => ByteString -> LB.ByteString -> WaiSession st (HTTP.Response LB.ByteString)
 post path = request methodPost path []
 
 -- | Perform a @PUT@ request to the application under test.
-put :: ByteString -> LB.ByteString -> WaiSession st (HTTP.Response LB.ByteString)
+put :: HasCallStack => ByteString -> LB.ByteString -> WaiSession st (HTTP.Response LB.ByteString)
 put path = request methodPut path []
 
 -- | Perform a @PATCH@ request to the application under test.
-patch :: ByteString -> LB.ByteString -> WaiSession st (HTTP.Response LB.ByteString)
+patch :: HasCallStack => ByteString -> LB.ByteString -> WaiSession st (HTTP.Response LB.ByteString)
 patch path = request methodPatch path []
 
 -- | Perform an @OPTIONS@ request to the application under test.
-options :: ByteString -> WaiSession st (HTTP.Response LB.ByteString)
+options :: HasCallStack => ByteString -> WaiSession st (HTTP.Response LB.ByteString)
 options path = request methodOptions path [] ""
 
 -- | Perform a @DELETE@ request to the application under test.
-delete :: ByteString -> WaiSession st (HTTP.Response LB.ByteString)
+delete :: HasCallStack => ByteString -> WaiSession st (HTTP.Response LB.ByteString)
 delete path = request methodDelete path [] ""
 
 -- | Perform a request to the application under test, with specified HTTP
 -- method, request path, headers and body.
-request :: Method -> ByteString -> [Header] -> LB.ByteString -> WaiSession st (HTTP.Response LB.ByteString)
+request :: HasCallStack => Method -> ByteString -> [Header] -> LB.ByteString -> WaiSession st (HTTP.Response LB.ByteString)
 request method path headers body = do
   port <- asks waiClientPort
   let req =
@@ -59,6 +60,7 @@ request method path headers body = do
   cj <- State.gets waiClientStateCookies
   let (req', cj') = insertCookiesIntoRequest req cj now
   State.modify' (\s -> s {waiClientStateCookies = cj'})
+
   performRequest req'
 
 -- | Perform a bare 'HTTP.Request'.
@@ -66,10 +68,14 @@ request method path headers body = do
 -- You can use this to make a request to an application other than the one
 -- under test.  This function does __not__ set the host and port of the request
 -- like 'request' does, but it does share a 'CookieJar'.
-performRequest :: HTTP.Request -> WaiSession st (HTTP.Response LB.ByteString)
+performRequest :: HasCallStack => HTTP.Request -> WaiSession st (HTTP.Response LB.ByteString)
 performRequest req = do
   man <- asks waiClientManager
-  resp <- liftIO $ httpRaw req man >>= traverse (fmap LB.fromChunks . brConsume)
+  respM <- liftIO $ try $ httpRaw req man >>= traverse (fmap LB.fromChunks . brConsume)
+  -- This is a naive way to restore a callstack from an exception which does not have one
+  resp <- case respM :: Either SomeException (Response LB.ByteString) of
+    Right resp -> pure resp
+    Left e -> liftIO $ throwIO e
   cj <- State.gets waiClientStateCookies
   now <- liftIO getCurrentTime
   let (cj', _) = updateCookieJar resp req now cj
